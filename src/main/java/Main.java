c31719aeb59921cc4745a1c94613472495a78a24
maksimmv <mvmmvch@gmail.com>
2025-01-24 22:48:18 +0300

    ver1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int speed;
+        int cars = 3;
+        ArrayList<Car> carArr = new ArrayList<>();
+        String carLeader ="";
+        for (int i = 0; i<cars; i++){
+            speed = 0;
+            Car car = new Car ();
+            System.out.println("Введите название "+ (i+1) + " автомобиля");
+            name = sc.nextLine();
+            car.name = name;
+            while (true){
+                System.out.println("Введите скорость от 0 до 250 км в ч "+ (i+1) + " автомобиля");
+                speed = Integer.parseInt(sc.nextLine());
+                if (speed>0 && speed<= 250){
+                    car.speed = speed;
+                    break;
+                } else {
+                    System.out.println("Не верное значение, попробуйте еще раз");
+                }
+            }
+            car.name =name;
+            car.speed = speed;
+
+            Race.raceLeader(car.name, car.speed);
+        }
+        System.out.println("Самая быстрая машина: "+Race.nameLeader);
+        sc.close();
+    }
+
+
+}
+
+
+class Car {
+    String name;
+    int speed;
+}
+class Race {
+    static String nameLeader;
+    static int  distance = 0;
+    public static void raceLeader (String name1, int speed1){
+        if (speed1*24> distance){
+            distance = speed1*24;
+            nameLeader = name1;
+        }
     }
 }
